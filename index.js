32860f89b8a4b5cf972e14a79c8ba4ad30c40171
you <wah0713@126.com>
2026-01-14 21:09:48 +0800

    fix 音频无法下载问题

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         微博一键下载(9宫格&&视频)
 // @namespace    https://github.com/wah0713/getWeiboResources
-// @version      2.4.1
+// @version      2.4.2
 // @description  一个兴趣使然的脚本，微博一键下载脚本。傻瓜式🐵(简单🍎、易用🧩、可靠💪)
 // @supportURL   https://github.com/wah0713/getWeiboResources/issues
 // @updateURL    https://greasyfork.org/scripts/454816/code/download.user.js
@@ -799,7 +799,7 @@
             return false
         }
 
-        if (!get(mediaRes, '_blob.type', '').startsWith('video')) {
+        if (!get(mediaRes, '_blob.type', '').startsWith('video') && !get(mediaRes, '_blob.type', '').startsWith('audio')) {
             const parser = new m3u8Parser.Parser();
             parser.push(await blobToText(mediaRes._blob));
             parser.end();
